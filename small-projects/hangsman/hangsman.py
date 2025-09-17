afdfe67c2c9d6eedf1b55faa770aa14759bf8a9c
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2025-09-16 21:47:01 -0400

    debugged

--- small-projects/hangsman/hangsman.py
+++ small-projects/hangsman/hangsman.py
@@ -15,10 +15,11 @@ lives = 6 #number of wrong guesses allowed
 print()
 print("\033[1m=-=-=-=- WELCOME TO HANGSMAN =-=-=-=-\033[0m")
 print(" ".join(display))
+print("\033[34mCATEGORY: Things you find in the kitchen. \033[0m")
 
 # main game loop
 while "_" in display and lives > 0:
-    guess = input("Guess a letter: ").lower()
+    guess = input("\nGuess a letter: ").lower()
     
     if guess in chosen_word:
         for i in range(word_length):
@@ -27,11 +28,11 @@ while "_" in display and lives > 0:
                 
     else:
         lives -= 1
-        print(f"\033[31m WRONG!\033[0m Lives left: {lives}")
+        print(f"\n\033[31m    WRONG!\033[0m Lives left: {lives}")
         
     print(" ".join(display))
     
 if "_" not in display:
-    print("YOU WON 🎉")
+    print("\n\033[32mYOU WON 🎉\033[0m Pretty smart eh?")
 else:
-    print(f"YOU LOST 💀 The word was '{chosen_word}'.")
+    print(f"\n\033[1m\33[31mYOU LOST 💀\033[0m\033[0m The word was \033[4m'{chosen_word}'\033[0m.")
