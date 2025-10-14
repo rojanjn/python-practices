55f204f0bf7d7350ee2ae521252612f00942894b
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2025-10-14 12:31:55 -0400

    feat: add number guessing game

---
+++ small-projects/number-guessing/number-guessing.py
@@ -0,0 +1,21 @@
+import random
+
+# computer picks a random number
+secretNum = random.randint(1,100)
+
+# intro
+print("\n\033[1m----------- WELCOME TO THE NUMBER-GUESSING GAME -----------\033[0m")
+print("\nIn this game, the computer will randomly choose a number between 1 to 100 and you have to guess the number. You have unlimited number of choices.")
+print("\n----------- Let the game begin... -----------")
+
+# get user's guesses
+guess = None
+while guess != secretNum:
+    guess = int(input("\nGuess a number between 1 to 100: "))
+    
+    if guess < secretNum:
+        print("\033[31mToo low!\033[0m Try again.")
+    elif guess > secretNum:
+        print("\033[31mToo high!\033[0m Try again.")
+    else:
+        print("\n\033[32mYOU WON 🎉\033[0m Honestly, you did better than ME!!!")
