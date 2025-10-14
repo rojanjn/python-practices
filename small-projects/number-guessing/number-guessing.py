57e7239898e6c84fcf689532d1a003bc3be7ed5a
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2025-10-14 12:34:57 -0400

    upgrade: added guess count

--- small-projects/number-guessing/number-guessing.py
+++ small-projects/number-guessing/number-guessing.py
@@ -10,12 +10,16 @@ print("\n----------- Let the game begin... -----------")
 
 # get user's guesses
 guess = None
+count = 0
+
 while guess != secretNum:
+    count += 1
+
     guess = int(input("\nGuess a number between 1 to 100: "))
     
     if guess < secretNum:
-        print("\033[31mToo low!\033[0m Try again.")
+        print(f"\033[31mToo low!\033[0m Attempt #{count}")
     elif guess > secretNum:
-        print("\033[31mToo high!\033[0m Try again.")
+        print(f"\033[31mToo high!\033[0m Attempt #{count}")
     else:
         print("\n\033[32mYOU WON 🎉\033[0m Honestly, you did better than ME!!!")
