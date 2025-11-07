5b767b708e90ebf44d95fd6536acc4566c14cfad
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2025-11-07 15:18:54 -0500

    fix: improve error messages and enhance user prompts in currency converter

--- currency-converter/currency-converter.py
+++ currency-converter/currency-converter.py
@@ -5,13 +5,24 @@ def convert_currency(amount, from_currency, to_currency):
     response = requests.get(url)
 
     if response.status_code != 200:
-        print("Error fetching exchange rate data.")
+        print("\nError fetching exchange rate data.")
         return
     
     data = response.json()
     rates = data["rates"]
 
     if to_currency.upper() not in rates:
-        print("Invalid target currency.")
+        print("\nInvalid target currency.")
         return
     
+    converted = amount * rates[to_currency.upper()]
+
+    print(f"{amount} {from_currency.upper()} = {converted:.2f} {to_currency.upper()}")
+
+# main menu of the program
+print("Welcome!")
+amount = float(input("\nPlease enter the amount you would like to convert: "))
+from_currency = input("\nEnter the currency you have (e.g. USD): ")
+to_currency = input("\nEnter the currency you want to convert to (e.g. CAD): ")
+
+convert_currency(amount, from_currency, to_currency)
