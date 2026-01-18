8274e56de8c3fe28df31647d12e3e1add92c797b
Rojan Jafarnezhad <rojanjafarnezhad@gmail.com>
2026-01-18 11:21:45 -0500

    feat: implement expense management menu in expense tracker

--- expense-tracker/expense-tracker.py
+++ expense-tracker/expense-tracker.py
@@ -1,5 +1,18 @@
-import sys
-import requests
+import argparse as ar
+from expenses import funcs
 
-def expenses:
+while True:
+    print("1. Add expense")
+    print("2. Update expense")
+    print("3. Delete expense")
+    print("4. View expenses")
+
+    choice = input("Please choose between 1 to 4: ")
+
+    if choice == 4:
+        print("1. View specific date")
+        print("2. View all")
+        print("3. exit to the main menu")
+        view_choice = input("Please choose between 1 to 3: ")
+
     
