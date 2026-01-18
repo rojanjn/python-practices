e6604092157587909db9fb4cf7e7ac2f2f240dda
Rojan Jafarnezhad <rojanjafarnezhad@gmail.com>
2026-01-18 11:47:19 -0500

    feat: initial fuctions

--- expense-tracker/expenses/funcs.py
+++ expense-tracker/expenses/funcs.py
@@ -2,5 +2,22 @@
 from datetime import date
 from typing import Any, Dict, List, Optional
 
-def menu:
-    
+def add_expense(id, category, amount, date):
+    pass
+
+def update_expense(id, ):
+    pass
+
+def delete_expense(id):
+    pass
+
+def view_expense():
+    def view_all():
+        pass
+
+    def view_month(month):
+        pass
+    pass
+
+def set_budget(budget):
+    pass
